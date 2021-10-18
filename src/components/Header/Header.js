a66e1c2314377f9e0ac9b75f89f2a5c3e5ac00e9
km00776 <kmurugathas@gmail.com>
2021-10-18 15:14:29 +0100

    added header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import {AppBar, Box, Toolbar} from '@material-ui/core';
+import React from 'react';
+import styles from './Header.css'
+
+
+const Header = () => {
+  return(
+    <header>
+       <AppBar style={{ height: "7%", backgroundColor: "rgb(14, 42, 71)" }}>
+          <h1 className={styles.heading}>Gym exercises</h1>
+       </AppBar>
+    </header>
+  )
+}
+
+export default Header;
