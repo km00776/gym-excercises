c350e42745c1e46082fe1088ca60d57693d695e0
Murugathas <2033658@cognizant.com>
2021-10-21 16:42:59 +0100

    added drawer - still need to fix alignment

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import Header from './components/Layout/Header/Header';
 import HomePage from './components/Layout/Pages/HomePage/HomePage';
 import Exercises from './components/Layout/Pages/ExercisesPage/ExercisePage'
-import { Route, Link } from "react-router-dom";
-
+import { Route  } from "react-router-dom";
+import MainDrawer from './components/Layout/Drawer/Drawer';
 import './App.css'
 function App () {
   return (
     <>
         <Header />
         <div className="pages">
-          <Route exact   path="/" component={HomePage} />
+          <Route exact path="/" component={HomePage} />
           <Route exact path="/Exercises" component = {Exercises} />
         </div>
     </>
--- src/components/Layout/Header/Header.js
+++ src/components/Layout/Header/Header.js
@@ -3,9 +3,56 @@ import React from 'react';
 import './Header.css'
 import MenuIcon from '@material-ui/icons/Menu';
 import AccountBoxIcon from '@material-ui/icons/AccountBox';
+import { styled, useTheme } from '@mui/material/styles';
+import IconButton from '@mui/material/IconButton';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import ListItem from '@mui/material/ListItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import Drawer from '@mui/material/Drawer';
+
+const drawerWidth = 240;
+
+const Main = styled('main', { shouldForwardProp: (prop) => prop !== 'open' })(
+  ({ theme, open }) => ({
+    flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: `-${drawerWidth}px`,
+    ...(open && {
+      transition: theme.transitions.create('margin', {
+        easing: theme.transitions.easing.easeOut,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+      marginLeft: 0,
+    }),
+  }),
+);
+
+
+
 
 
 const Header = () => {
+  const [open, setOpen] = React.useState(false);
+  const theme = useTheme();
+
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
   // create a useState
 
 
@@ -13,7 +60,16 @@ const Header = () => {
     <header>
        <AppBar elevation={0} style={{ height: "7%", backgroundImage: `linear-gradient( rgba(0, 79, 240, 3), rgba(0, 79, 240, 3))`}}>
           <div className="full-heading">
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            edge="start"
+            sx={{ mr: 1, ...(open && { display: 'none' }) }}
+           
+          > 
              <MenuIcon className="menu-icon" />
+             </IconButton>
               <div className="title-icon">
                 <img className="menu-logo" alt="squat" src="https://i.imgur.com/ilmD2vQ.png"></img>
                 <h2 className="menu-header">K-Workz</h2>
@@ -21,8 +77,62 @@ const Header = () => {
               <AccountBoxIcon className="menu-account-box-icon" />
           </div>
        </AppBar>
+       <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          '& .MuiDrawer-paper': {
+            width: drawerWidth,
+            boxSizing: 'border-box',
+          },
+        }}
+        variant="persistent"
+        anchor="left"
+        open={open}
+      >
+        <DrawerHeader>
+          <IconButton onClick={handleDrawerClose}>
+            {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+          </IconButton>
+        </DrawerHeader>
+        <Divider />
+        <List>
+          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+        <Divider />
+        <List>
+          {['All mail', 'Trash', 'Spam'].map((text, index) => (
+            <ListItem button key={text}>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
     </header>
   )
 }
+
+
+
+const DrawerHeader = styled('div')(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+  justifyContent: 'flex-end',
+}));
+
+
 
 export default Header;
